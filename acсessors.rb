b24789ee9949c77070d1ab02bad0ec64e1111e72
Sergio <nakul.sv@gmail.com>
2019-04-25 08:48:08 +0300

    lesson10 - accessor method

---
+++ acсessors.rb
@@ -0,0 +1,21 @@
+module Acсessors
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def attr_accessor_with_history(*names)
+      names.each do |name|
+        var_name = "@#{name}".to_sym
+        define_method(name) { instance_variable_get(var_name) }
+        define_method("#{name}=".to_sym) do |value|
+          instance_variable_set(var_name, value)
+          @history ||= {}
+          @history[var_name] ||= []
+          @history[var_name] << value
+        end
+        define_method("#{name}_history") { @history[var_name] }
+      end      
+    end
+  end
+end
